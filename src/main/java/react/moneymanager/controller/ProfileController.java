62b946ab5140c0a9f3fbb715d3fc730d3816bfff
NayAungLin910 <nayaunglin910@gmail.com>
2025-09-30 00:04:17 +0900

    Add Activate Token Request

--- src/main/java/react/moneymanager/controller/ProfileController.java
+++ src/main/java/react/moneymanager/controller/ProfileController.java
@@ -3,9 +3,7 @@ package react.moneymanager.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import react.moneymanager.dto.ProfileDto;
 import react.moneymanager.service.ProfileService;
 
@@ -18,5 +16,15 @@ public class ProfileController {
     public ResponseEntity<ProfileDto> registerProfile(@RequestBody ProfileDto profileDto) {
         ProfileDto registeredProfile = profileService.registerProfile(profileDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(registeredProfile);
+    }
+
+    @GetMapping("/activate")
+    public ResponseEntity<String> activateProfile(@RequestParam String token) {
+        boolean isActivated = profileService.activateProfile(token);
+        if(isActivated) {
+            return ResponseEntity.ok("Profile activated successfully!");
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Activation token not found or already used!");
+        }
     }
 }
