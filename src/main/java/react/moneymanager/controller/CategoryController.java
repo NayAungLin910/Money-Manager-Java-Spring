5f15146902f3a0ce0f23a8c8486bc3f8501d5d52
NayAungLin910 <nayaunglin910@gmail.com>
2025-11-06 23:57:45 +0900

    Add CategoryController, Add Category Methods

---
+++ src/main/java/react/moneymanager/controller/CategoryController.java
@@ -0,0 +1,29 @@
+package react.moneymanager.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import react.moneymanager.dto.CategoryDTO;
+import react.moneymanager.service.CategoryService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> saveCategory(@RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO savedCategory = this.categoryService.saveCategory(categoryDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> getCategories() {
+        List<CategoryDTO> categories = categoryService.getCategoriesForCurrentUser();
+        return ResponseEntity.ok(categories);
+    }
+}
--- src/main/java/react/moneymanager/service/CategoryService.java
+++ src/main/java/react/moneymanager/service/CategoryService.java
@@ -1,14 +1,17 @@
 package react.moneymanager.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import react.moneymanager.dto.CategoryDTO;
 import react.moneymanager.entity.CategoryEntity;
 import react.moneymanager.entity.ProfileEntity;
 import react.moneymanager.repository.CategoryRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,11 +23,18 @@ public class CategoryService {
     public CategoryDTO saveCategory(CategoryDTO categoryDTO) {
         ProfileEntity currentProfile = profileService.getCurrentProfile();
         if (categoryRepository.existsByNameAndProfileId(categoryDTO.getName(), currentProfile.getId())) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "Category with this name alreayd exists!");
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Category with the name already exists!");
         }
         CategoryEntity categoryEntity = toEntity(categoryDTO, currentProfile);
         CategoryEntity newCategory = categoryRepository.save(categoryEntity);
         return toDto(newCategory);
+    }
+
+    // get categories for current user
+    public List<CategoryDTO> getCategoriesForCurrentUser() {
+        ProfileEntity profile = profileService.getCurrentProfile();
+        List<CategoryEntity> categories = categoryRepository.findByProfileId(profile.getId());
+        return categories.stream().map(this::toDto).toList();
     }
 
     // helper methods
